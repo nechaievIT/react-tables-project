3ce987f42dcc6e633963c68a597db8fc59f1b77b
Yurii Nechaiev <nechaiev.it@gmail.com>
2022-07-23 18:20:20 +0300

    Test task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BasicTable } from "./tables/BasicTable"
+import { ColumnHiding } from "./tables/ColumnHiding"
+import { FilteringTable } from "./tables/FiltertingTable"
+import { PaginationTable } from "./tables/PaginationTable"
+import { RowSelection } from "./tables/RowSelection"
+import { SortingTable } from "./tables/SortingTable"
 
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <BasicTable />
+      <div className='space'/>
+      <ColumnHiding />
+      <div className='space'/>
+      <FilteringTable />
+      <div className='space'/>
+      <PaginationTable />
+      <div className='space'/>
+      <RowSelection />
+      <div className='space'/>
+      <SortingTable />
     </div>
   );
 }
---
+++ src/tables/TableStyles.js
@@ -0,0 +1,59 @@
+import styled from 'styled-components'
+
+export const Styles = styled.div`
+  .table {
+    border: 1px solid #ddd;
+
+    .tr {
+      :last-child {
+        .td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    .th,
+    .td {
+      padding: 5px;
+      border-bottom: 1px solid #ddd;
+      border-right: 1px solid #ddd;
+      background-color: #fff;
+      overflow: hidden;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+
+    &.sticky {
+      overflow: scroll;
+      .header {
+        position: sticky;
+        z-index: 1;
+        width: fit-content;
+      }
+
+      .header {
+        top: 0;
+        box-shadow: 0px 3px 3px #bbb;
+      }
+
+      .body {
+        position: relative;
+        z-index: 0;
+      }
+
+      [data-sticky-td] {
+        position: sticky;
+      }
+
+      [data-sticky-last-left-td] {
+        box-shadow: 2px 0px 3px #bbb;
+      }
+
+      [data-sticky-first-right-td] {
+        box-shadow: -2px 0px 3px #bbb;
+      }
+    }
+  }
+`
